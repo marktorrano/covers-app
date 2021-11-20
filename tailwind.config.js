8e3146d3ca1518c89d018eb9cc66a89789bd903e
Mark Torrano <markstorrano@gmail.com>
2021-11-19 17:14:31 -0800

    Added config files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#043f47",
+        secondary: "#fe4f00",
+        tertiary: "#f8f8f8"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
